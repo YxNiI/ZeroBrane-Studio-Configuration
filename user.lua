b711f2506c0709b52281969a894be0ec45cc22da
YxNiI <107792920+YxNiI@users.noreply.github.com>
2024-11-28 12:33:56 +0100

    added more custom menu-keybindings and documentaton for editor- and menu-keybindings

--- user.lua
+++ user.lua
@@ -1,10 +1,9 @@
 --[[--
- Use this file to specify **System** preferences.
- Review [examples](+C:\Users\Lyra\my_tools\ZeroBraneStudio\cfg\user-sample.lua) or check [online documentation](http://studio.zerobrane.com/documentation.html) for details.
+[YxNill's](https://github.com/YxNiI) configuration-file for [ZeroBrane Studio](https://studio.zerobrane.com/)
 --]]--
 
 
--- allgemein
+-- [editor-preferences](https://studio.zerobrane.com/doc-editor-preferences)
 editor.tabwidth = 1
 editor.fold = false
 editor.fontname = "Picotron"
@@ -13,12 +12,31 @@ editor.fontsize = 16
 
 -- keybindings
 
--- menu-keybindings
-keymap[ID.EXIT] = "Ctrl-Shift-Q"
-keymap[ID.CLOSE] = "Ctrl-Q"
-keymap[ID.GOTODEFINITION] = "Ctrl-J"
+-- [menu-keybindings](https://github.com/pkulchenko/ZeroBraneStudio/blob/master/src/editor/keymap.lua)
+keymap[ID.EXIT] = "Ctrl-Shift-Q"          -- shutdown-IDE
+keymap[ID.CLOSE] = "Ctrl-Q"               -- close tab
+keymap[ID.GOTODEFINITION] = "Ctrl-J"      -- go to definition
+keymap[ID.VIEWFILETREE] = "Alt-1"         -- open file-explorer
+keymap[ID.VIEWOUTPUT] = "Alt-4"           -- open runtime-console-view
+keymap[ID.VIEWCALLSTACK] = "Alt-5"        -- open callstack
+keymap[ID.AUTOCOMPLETE] = "Ctrl-SPACE"    -- open suggestions for autocompletion
 
--- editor-keybindings
+
+-- [editor-keybindings](https://studio.zerobrane.com/doc-editor-keyboard-shortcuts)
+
+--[[--
+documentation for changing the bindings:
+- [how-to](https://studio.zerobrane.com/doc-editor-preferences#keyboard-shortcuts)
+- [special codes](https://www.scintilla.org/ScintillaDoc.html#KeyBindings)
+- [commands](https://www.scintilla.org/ScintillaDoc.html#KeyboardCommands)
+
+Ctrl-W    -> move cursor to the start of the line
+Ctrl-E    -> move cursor to the end of the line
+Ctrl-Up   -> move cursor to file-top
+Ctrl-Down -> move cursor to file-top
+Alt-Up    -> move cursor to last paragraph
+Alt-Down  -> move cursor to next paragraph
+--]]--
 editor.keymap[#editor.keymap + 1] = {('W'):byte(), wxstc.wxSTC_SCMOD_CTRL, wxstc.wxSTC_CMD_HOME}
 editor.keymap[#editor.keymap + 1] = {('E'):byte(), wxstc.wxSTC_SCMOD_CTRL, wxstc.wxSTC_CMD_LINEEND}
 editor.keymap[#editor.keymap + 1] = {wxstc.wxSTC_KEY_UP, wxstc.wxSTC_SCMOD_CTRL, wxstc.wxSTC_CMD_DOCUMENTSTART}
@@ -28,7 +46,7 @@ editor.keymap[#editor.keymap + 1] = {wxstc.wxSTC_KEY_DOWN, wxstc.wxSTC_SCMOD_ALT
 
 
 
--- style-attributes
+-- style-attributes and color-schemes -> WIP
 
 -- Pico-8-colors
 local colors = {}
@@ -71,7 +89,7 @@ favourite standard color-themes:
 - SolarizedDark
 - NotepadPlusPlus
 - SciTeLuaIDE
-]]--
+--]]--
 styles = loadfile('cfg/tomorrow.lua')('TomorrowNightBlue')
 stylesoutshell = styles -- apply the same scheme to Output/Console windows
 styles.auxwindow = styles.text -- apply text colors to auxiliary windows
